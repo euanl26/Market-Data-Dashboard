878f8aa5e6e3b3cc4f43aebf683af446bbd558f8
Euan Levey-James <euanlevey@icloud.com>
2025-12-11 12:24:25 +0000

    Initial API implementation testing and setup .gitginore

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
 import streamlit as st
+import os
+from massive import RESTClient
+from dotenv import load_dotenv
 
+load_dotenv()
+
+MASSIVE_API_KEY = os.getenv('MASSIVE_API_KEY')
+
+client = RESTClient(api_key=MASSIVE_API_KEY)
+
+def request():
+    ticker = input("Enter ticker: ")
+    quote = client.get_previous_close_agg(
+        ticker=ticker,
+        adjusted=True
+    )
+    print(quote)
+
+request()
